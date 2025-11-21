90601b0a72643364110285a52eea9970aee9eefe
@rakesh.btkol <rakesh.btkol@gmail.com>
2025-11-21 23:50:48 +0530

    fix: update Zoho email transport configuration

--- email/zoho.js
+++ email/zoho.js
@@ -1,11 +1,14 @@
 const nodemailer = require("nodemailer");
 console.log("Zoho transporter initialized", process.env.ZOHO_EMAIL, process.env.ZOHO_PASSWORD);
 module.exports = nodemailer.createTransport({
-  host: "smtppro.zoho.in",
-  port: 465,
-  secure: true,
+  //host: "smtppro.zoho.in",
+  host: "smtp.zeptomail.in",
+  //port: 465,
+  port: 587,
+  // secure: true,
   auth: {
-    user: process.env.ZOHO_EMAIL,
+    //user: process.env.ZOHO_EMAIL,
+    user: 'emailapikey',
     pass: process.env.ZOHO_PASSWORD
   }
 });
